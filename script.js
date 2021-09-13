91113e211ba6892fb2a5b815001661f3102319ac
Reimen123 <40213088+Reimen123@users.noreply.github.com>
2021-09-13 20:29:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function diamond(l, i = 1) {
+    //Nodefinējam constantes
     const
         STAR = '*',
         SPACE = ' ',
-        LINE = SPACE.repeat((l - i) / 2) + STAR.repeat(i);
+        LINE = SPACE.repeat((l - i) / 2) + STAR.repeat(i); // Noradam cik rezies atkārtot formulu, i ir 7
 
+    //printējam consolē līnijas, kas ir padotais strings kurš tika definēts kā formula
     console.log(LINE);
+    // pārvaude lai printētu apakšu
     if (i >= l) return;
+    //izsaucam function atkal ar + 2 lai printētu vairāk zvaigznītes
     diamond(l, i + 2);
     console.log(LINE);
 }
-
+//Izsaucam funkciju daimonds
 diamond(7);
